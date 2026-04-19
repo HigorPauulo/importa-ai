db8fba67c0d41d89ec35cee12d1606b9f7f165c1
Higor Paulo <h.pauloc12@gmail.com>
2026-04-19 20:31:56 -0300

    feat: estrutura e layout visual da tela de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './features/auth/pages/LoginPage'
 function App() {
   return (
     <>
-      <h1>Importa Aí</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
